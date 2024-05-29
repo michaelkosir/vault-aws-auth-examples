a003e2e42fed1a0c268132dbb9e376e0e99dd1ca
Michael Kosir <michael.kosir@hashicorp.com>
2024-05-29 04:26:33 -0700

    comments

--- tf/code/extension/main.py
+++ tf/code/extension/main.py
@@ -5,6 +5,10 @@ def handler(event, context):
     with open("/tmp/vault/secret.json") as fp:
         secrets = json.load(fp)
 
+    # alternative (Recommended)
+    # Make unauthenticated requests to the extension's
+    # local proxy server at http://127.0.0.1:8200
+
     # do stuff with secrets
 
     # return secrets for demo purposes
